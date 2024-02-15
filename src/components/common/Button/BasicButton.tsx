b342cdaad5744aba703c7c13a50f14a90f45e739
斎藤泉 <saitoizumi@saitouizuminoMacBook-Air.local>
2024-02-15 17:39:01 +0900

    色々追加

--- src/components/common/Button/BasicButton.tsx
+++ src/components/common/Button/BasicButton.tsx
@@ -1,7 +1,24 @@
+"use client";
+import styled from "styled-components";
+import { GlobalPrimary } from "../../../../build/js/tokens";
+
 type Props = {
   text: string;
 };
 
 export const BasicButton = ({ text }: Props) => {
-  return <button>{text}</button>;
+  return (
+    <ButtonStyle>
+      <button>{text}</button>
+    </ButtonStyle>
+    // <button className="basic-button">{text}</button>
+  );
 };
+
+const ButtonStyle = styled.div`
+  > button {
+    background-color: ${GlobalPrimary};
+    border-radius: 50px;
+    padding: 10px 20px;
+  }
+`;
