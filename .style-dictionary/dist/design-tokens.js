1f74fcacfe3c3501d465090d209295de49480502
saitoizumi <saitoizumi@users.noreply.github.com>
2024-02-15 09:34:02 +0000

    Update generated style files

--- .style-dictionary/dist/design-tokens.js
+++ .style-dictionary/dist/design-tokens.js
@@ -1,7 +1,10 @@
 /**
  * Do not edit directly
- * Generated on Thu, 15 Feb 2024 08:54:23 GMT
+ * Generated on Thu, 15 Feb 2024 09:34:01 GMT
  */
 
-export const GlobalPrimary = "#42ff63";
-export const GlobalSecondary = "#a4a4a4";
+export const Primary = "#42ff63";
+export const Secondary = "#a4a4a4";
+export const Third = "#ed0505";
+export const FontColor = "#3c3c3c";
+export const TokenSetOrder0 = "global";
